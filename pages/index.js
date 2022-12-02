0045997bb5a93c65ede33e157ec5cf7881c30d7c
cakeGit <65340665+cakeGit@users.noreply.github.com>
2022-12-02 17:45:59 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,11 @@ import BlogCard from "../components/BlogCard";
 import PageMetadata from "../components/PageMetadata";
 
 const graphcms = new GraphQLClient(
+  //Api key do noy touch! Only change if trasfering hygraph model
   "https://api-eu-west-2.hygraph.com/v2/clalhyk7r20kx01tc9qipb81z/master"
 );
 
+//Needed to pull data from hygraph
 const QUERY = gql`
   {
     posts {
