5259868e06a698080a891f8bac97887a2975ed61
Sebastian Sexton <sebastiansexton15@gmail.com>
2022-12-10 11:47:02 +0000

    updated 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,27 +1,24 @@
-import Link from "next/link";
+// 404.js
 import Head from "next/head";
 import styles from "../styles/Error.module.css";
+import Link from 'next/link'
 
-function Error() {
-  return (
-    <div className={styles.container}>
-        <Head>
-            <title>
-                Error | 404
-            </title>
-        </Head>
-
+export default function FourOhFour() {
+  return <>
+    <Head>
+        <title>
+            Error | 404
+        </title>
+    </Head>
         <h2 className={styles.header}>
             Oops, somethings gone wrong...
         </h2>
-        <h1 className={styles.code}>
-            404
-        </h1>
-            <p className={styles.return}>
-                File not found : Try going back to the <Link href="/" title="Return home">homepage</Link>
-            </p>
-    </div>
-  )
+        <h1>404 - Page Not Found</h1>
+    <Link href="/">
+        <a>
+            Go back home
+         </a>
+    </Link>
+  </>
 }
- 
-export default Error;
+
