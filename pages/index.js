df2d8e36d6254ec21273eb8b2434030bc1fc2eed
Sebastian Sexton <sebastiansexton15@gmail.com>
2022-12-10 11:56:27 +0000

    TESTING

--- pages/index.js
+++ pages/index.js
@@ -77,6 +77,24 @@ export default function Home({ posts }) {
             slug={post.slug}
           />
         ))}
+
+<form name="fileForm" enctype="multipart/form-data" data-netlify="true">
+      <p>
+        <label>
+          <span>Name:</span>
+          <input name="name" type="text"/>
+        </label>
+      </p>
+        <p>
+          <label>
+            <span>Add file:</span>
+            <input name="file" type="file"/>
+          </label>
+         </p>
+         <button>Submit</button>
+          </form>
+        <p class="result"></p>
+
       </main>
     </div>
     
