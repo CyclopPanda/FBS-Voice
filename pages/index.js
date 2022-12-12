e90c4a24a21817e38b2634b4799ece1157571356
Sebastian Sexton <sebastiansexton15@gmail.com>
2022-12-12 11:55:30 +0000

    fixed

--- pages/index.js
+++ pages/index.js
@@ -110,21 +110,6 @@ export default function Home({ posts }) {
       
     </div>
   );
-};
-
-document.forms.fileForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const result = document.querySelector(".result");
-  fetch("/", {
-    body: new FormData(event.target),
-    method: "POST",
-  })
-    .then(() => {
-      result.innerText = "Success";
-    })
-    .catch((error) => {
-      result.innerText = `Failed: ${error}`;s
-    });
-});
+}
 
 //created by CyclopPanda, cakGit and Thomas
