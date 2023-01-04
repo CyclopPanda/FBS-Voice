cb1e804b173eed2da1f5e4a0e684666ed7c56187
Sebastian Sexton <sebastiansexton15@gmail.com>
2023-01-04 20:04:31 +0000

    fixed some webpack errors

--- pages/category/School.js
+++ pages/category/School.js
@@ -1,4 +1,4 @@
-import Head from "../next/head";
+import Head from "next/head";
 import styles from "../../styles/Home.module.css";
 import { GraphQLClient, gql } from "graphql-request";
 import BlogCard from "../../components/BlogCard";
