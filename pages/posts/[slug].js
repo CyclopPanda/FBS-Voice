de7d0ab0665c2bdef0905dbbb71db03dc8d30222
Sebastian Sexton <sebastiansexton15@gmail.com>
2023-01-14 19:46:08 +0000

    added title to posts

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,4 +1,5 @@
 import styles from "../../styles/Slug.module.css";
+import DefaultHead from "components/DefaultHead"
 import moment from "moment";
 
 const Content = require("../../components/Content");
@@ -26,40 +27,40 @@ export default function BlogPost({ post }) {
   return (
     <main className={styles.blog}>
 
-      <div
-        className={styles.titleContainer}
-        style={{backgroundImage: "url(" + post.coverPhoto.url + ")"}}
+        <div
+          className={styles.titleContainer}
+          style={{ backgroundImage: "url(" + post.coverPhoto.url + ")" }}
         >
-        
-        <div className={styles.title}>
-          <h1>
-            {post.title}
-          </h1>
 
-          <h5>
-            {post.category}
-          </h5>
+          <div className={styles.title}>
+            <h1>
+              {post.title}
+            </h1>
 
-          <div className={styles.authorDetails}>
-            <img src={post.author.avatar.url} alt={post.author.name} />
-            <div className={styles.authorText}>
-              <h6>By {post.author.name} </h6>
-              <h6 className={styles.date}>
-                {moment(post.datePublished).format("MMMM d, YYYY")}
-              </h6>
+            <h5>
+              {post.category}
+            </h5>
+
+            <div className={styles.authorDetails}>
+              <img src={post.author.avatar.url} alt={post.author.name} />
+              <div className={styles.authorText}>
+                <h6>By {post.author.name} </h6>
+                <h6 className={styles.date}>
+                  {moment(post.datePublished).format("MMMM d, YYYY")}
+                </h6>
+              </div>
             </div>
           </div>
+
+
         </div>
-        
 
-      </div>
+        <div
+          className={styles.content}
+          dangerouslySetInnerHTML={{ __html: post.content.html }}
+        >
+        </div>
 
-      <div
-        className={styles.content}
-        dangerouslySetInnerHTML={{ __html: post.content.html }}
-      >
-      </div>
-      
-    </main>
+      </main>
   );
 }
